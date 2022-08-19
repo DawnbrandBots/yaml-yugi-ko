3e14bb159cbb805b3447959d77eebbf7ab4f25ce
kevinlul <6320810+kevinlul@users.noreply.github.com>
2022-08-19 00:35:55 -0400

    Create job script

---
+++ src/job.py
@@ -0,0 +1,65 @@
+# SPDX-FileCopyrightText: © 2022 Kevin Lu
+# SPDX-Licence-Identifier: AGPL-3.0-or-later
+import json
+import os
+import sys
+import time
+from typing import Any, Dict, Optional
+
+from httpx import Client, HTTPStatusError, RequestError
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import LiteralScalarString
+
+from scraper import get_card
+
+
+def get_card_retry(client: Client, konami_id: int) -> Optional[Dict[str, Any]]:
+    for retry in range(5):
+        try:
+            card = get_card(client, konami_id)
+            if card:
+                return card._asdict()
+            else:
+                print(f"{konami_id}\tNOT FOUND", flush=True)
+                return
+        except HTTPStatusError as e:
+            print(f"{konami_id}\tFAIL {e.response.status_code}", flush=True)
+            return
+        except RequestError as e:
+            print(f"{konami_id}\tTRY {retry}\t{e}", flush=True)
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit(f"Usage: {sys.argv[0]} <cards.json> [output directory]")
+
+    file = sys.argv[1]
+    with open(file) as handle:
+        cards = json.load(handle)
+    print(f"Found {len(cards)} cards.", flush=True)
+    ids = [card["konami_id"] for card in cards if card.get("konami_id")]
+    print(f"Using {len(cards)} ids.", flush=True)
+
+    if len(sys.argv) > 2:
+        os.chdir(sys.argv[2])
+
+    with Client(http2=True) as client:
+        yaml = YAML()
+        for kid in ids:
+            if os.path.exists(f"{kid}.yaml"):
+                print(f"{kid}\tSKIP", flush=True)
+                continue
+
+            card = get_card_retry(client, kid)
+
+            card["text"] = LiteralScalarString(card["text"])
+            if card["pendulum"]:
+                card["pendulum"] = LiteralScalarString(card["pendulum"])
+            else:
+                card.pop("pendulum")
+
+            with open(f"{kid}.yaml", mode="w", encoding="utf-8") as out:
+                yaml.dump(card, out)
+            print(f"{kid}.yaml", flush=True)
+            time.sleep(1)
