77586724bc6d33f9d46782716b3832e327bd509e
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2023-08-09 23:03:54 -0400

    as_csv: toggle pendulum column, write column names

--- src/as_csv.py
+++ src/as_csv.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2022 Kevin Lu
+# SPDX-FileCopyrightText: © 2022–2023 Kevin Lu
 # SPDX-Licence-Identifier: AGPL-3.0-or-later
 from csv import DictWriter
 import os
@@ -9,13 +9,17 @@ from ruamel.yaml import YAML
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        sys.exit(f"Usage: {sys.argv[0]} <yaml_dir> <output.csv>")
+        sys.exit(f"Usage: {sys.argv[0]} <yaml_dir> <output.csv> [--with-pendulum]")
 
-    _, yaml_dir, output_file = sys.argv
+    _, yaml_dir, output_file, with_pendulum = sys.argv
 
     yaml = YAML()
+    columns = ["konami_id", "name", "text"]
+    if with_pendulum:
+        columns += ["pendulum"]
     with open(output_file, "w") as csv_file:
-        writer = DictWriter(csv_file, ["konami_id", "name", "text", "pendulum"])
+        writer = DictWriter(csv_file, columns)
+        writer.writeheader()
         for filename in os.listdir(yaml_dir):
             if filename.endswith(".yaml"):
                 konami_id = int(os.path.splitext(filename)[0])
